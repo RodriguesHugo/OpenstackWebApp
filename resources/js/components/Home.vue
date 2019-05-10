481f2956e82b2b5c5afa7b2acbd8254e942418f8
Vingas <2161319@my.ipleiria.pt>
2019-05-10 17:53:54 +0100

    missing axios Login page

--- resources/js/components/Home.vue
+++ resources/js/components/Home.vue
@@ -1,6 +1,41 @@
 <template>
-  <p>home</p>
+  <v-container>
+    <v-flex md4>
+      <form>
+        <v-text-field v-model="email" label="E-mail" required></v-text-field>
+      </form>
+    </v-flex>
+    <v-flex md4>
+      <form>
+        <v-text-field v-model="password" label="Password" type="password" required></v-text-field>
+        <v-btn color="success" @click="login">Login</v-btn>
+      </form>
+    </v-flex>
+  </v-container>
 </template>
 <script>
-export default {};
+export default {
+  data() {
+    return {
+      email: "",
+      password: ""
+    };
+  },
+  methods: {
+    login() {
+      let credenciais = {
+        mail: this.email,
+        password: this.password
+      };
+      console.log(credenciais);
+      axios
+        .post("api/login", credenciais)
+        .then(response => {})
+        .catch(error => {
+          console.log(error);
+          console.log(error.response.data.message);
+        });
+    }
+  }
+};
 </script>
